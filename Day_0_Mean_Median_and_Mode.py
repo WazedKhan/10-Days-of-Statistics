623c065c9449d3db36815e8d5e9a51ec07e0014d
WazedKhan <51821594+WazedKhan@users.noreply.github.com>
2021-08-15 13:03:00 +0600

    mean median

--- Day_0_Mean_Median_and_Mode.py
+++ Day_0_Mean_Median_and_Mode.py
@@ -1,1 +1,18 @@
-print("hello")
+def mean(arr):
+    print(sum(arr)/10)
+
+
+def median(arr):
+    if len(arr) % 2 != 0:
+        print(round(len(arr)/2)+1)
+    else:
+        mid = arr[(int(len(arr)/2)-1)] + arr[(int(len(arr)/2))]
+        print(mid/2)
+
+
+arr = sorted(list(map(
+    int, '64630 11735 14216 99233 14470 4978 73429 38120 51135 67060'.split(' '))))
+
+
+mean(arr)
+median(arr)
